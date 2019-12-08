41f5c1ee6c3609ae59a0dc5bb0cfa90d99acb75b
archersama <946971688@qq.com>
2019-12-07 21:35:43 -0600

    Add files via upload

---
+++ code/d2lzh_tensorflow2/utils.py
@@ -0,0 +1,18 @@
+import collections
+import math
+import os
+import random
+import sys
+import tarfile
+import time
+import json
+import zipfile
+from PIL import Image
+from collections import namedtuple
+from matplotlib import pyplot as plt
+import tensorflow
+from tensorflow import nn
+import torch.nn.functional as F
+import numpy as np
+from IPython import display
+
