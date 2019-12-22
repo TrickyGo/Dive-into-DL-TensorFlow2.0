2add353f559ff5c5a4d5b8a79d90b88195781097
swordfaith <mindsculptor@yeah.net>
2019-12-23 00:19:09 +0800

    add chapter3 functions to d2lzh_tf2 utils.py and declare current version as 0.0.1

--- code/d2lzh_tensorflow2/__init__.py
+++ code/d2lzh_tensorflow2/__init__.py
@@ -1,1 +1,3 @@
 from .utils import *
+
+__version__ = '0.0.1'
--- code/d2lzh_tensorflow2/utils.py
+++ code/d2lzh_tensorflow2/utils.py
@@ -13,7 +13,114 @@ from matplotlib import pyplot as plt
 import tensorflow
 from tensorflow import nn
 from tensorflow import keras
-import torch.nn.functional as F
 import numpy as np
 from IPython import display
 
+VOC_CLASSES = ['background', 'aeroplane', 'bicycle', 'bird', 'boat',
+               'bottle', 'bus', 'car', 'cat', 'chair', 'cow',
+               'diningtable', 'dog', 'horse', 'motorbike', 'person',
+               'potted plant', 'sheep', 'sofa', 'train', 'tv/monitor']
+
+VOC_COLORMAP = [[0, 0, 0], [128, 0, 0], [0, 128, 0], [128, 128, 0],
+                [0, 0, 128], [128, 0, 128], [0, 128, 128], [128, 128, 128],
+                [64, 0, 0], [192, 0, 0], [64, 128, 0], [192, 128, 0],
+                [64, 0, 128], [192, 0, 128], [64, 128, 128], [192, 128, 128],
+                [0, 64, 0], [128, 64, 0], [0, 192, 0], [128, 192, 0],
+                [0, 64, 128]]
+
+# ###################### 3.2 ############################
+def use_svg_display():
+    """Use svg format to display plot in jupyter"""
+    display.set_matplotlib_formats('svg')
+
+def set_figsize(figsize=(3.5, 2.5)):
+    use_svg_display()
+    # 设置图的尺寸
+    plt.rcParams['figure.figsize'] = figsize
+
+def data_iter(batch_size, features, labels):
+    features = np.array(features)
+    labels = np.array(labels)
+    num_examples = len(features)
+    indices = list(range(num_examples))
+    random.shuffle(indices)  # 样本的读取顺序是随机的
+    for i in range(0, num_examples, batch_size):
+        j = np.array(indices[i:min(i + batch_size, num_examples)]) # 最后一次可能不足一个batch
+        yield features[j], labels[j]
+
+def linreg(X, w, b):
+    return tf.matmul(X, w) + b
+
+def squared_loss(y_hat, y): 
+    # 注意这里返回的是向量, 另外, pytorch里的MSELoss并没有除以 2
+    return (y_hat - tf.reshape(y, y_hat.shape)) ** 2 / 2
+
+def sgd(params, lr, batch_size, tape):
+    # 为了和原书保持一致，这里除以了batch_size，但是应该是不用除的，因为一般用PyTorch计算loss时就默认已经
+    # 沿batch维求了平均了。
+    for param in params:
+        param.assign_sub(lr * tape.gradient(l, param) / batch_size)
+
+# ###################### 3.5 ############################
+def get_fashion_mnist_labels(labels):
+    text_labels = ['t-shirt', 'trouser', 'pullover', 'dress', 'coat',
+                   'sandal', 'shirt', 'sneaker', 'bag', 'ankle boot']
+    return [text_labels[int(i)] for i in labels]
+
+def show_fashion_mnist(images, labels):
+    use_svg_display()
+    # 这里的_表示我们忽略（不使用）的变量
+    _, figs = plt.subplots(1, len(images), figsize=(12, 12))
+    for f, img, lbl in zip(figs, images, labels):
+        f.imshow(img.reshape((28, 28)))
+        f.set_title(lbl)
+        f.axes.get_xaxis().set_visible(False)
+        f.axes.get_yaxis().set_visible(False)
+    plt.show()
+
+# ###################### 3.6 ############################
+def train_ch3(net, train_iter, test_iter, loss, num_epochs, batch_size,
+              params=None, lr=None, trainer=None):
+    for epoch in range(num_epochs):
+        train_l_sum, train_acc_sum, n = 0.0, 0.0, 0
+        for X, y in train_iter:
+            with tf.GradientTape(persistent=True) as tape:
+                y_hat = net(X)
+                l = tf.reduce_sum(loss(y_hat, y))
+            
+            grads = tape.gradient(l, params)
+            if trainer is None:
+                for param in params:
+                    param.assign_sub(lr * tape.gradient(l, param) / batch_size)
+            else:
+                trainer.apply_gradients(zip(grads, params))  # “softmax回归的简洁实现”一节将用到
+
+            y = tf.cast(y, dtype=tf.float32)
+            train_l_sum += l.numpy()
+            train_acc_sum += tf.reduce_sum(tf.cast(tf.argmax(y_hat, axis=1) == tf.cast(y, dtype=tf.int64), dtype=tf.int64)).numpy()
+            n += y.shape[0]
+        test_acc = evaluate_accuracy(test_iter, net)
+        print('epoch %d, loss %.4f, train acc %.3f, test acc %.3f'
+              % (epoch + 1, train_l_sum / n, train_acc_sum / n, test_acc))
+
+# ###################### 3.11 ############################
+def semilogy(x_vals, y_vals, x_label, y_label, x2_vals=None, y2_vals=None,
+             legend=None, figsize=(3.5, 2.5)):
+    set_figsize(figsize)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.semilogy(x_vals, y_vals)
+    if x2_vals and y2_vals:
+        plt.semilogy(x2_vals, y2_vals, linestyle=':')
+        plt.legend(legend)
+    plt.show()
+
+
+# ###################### 3.13 ############################
+def evaluate_accuracy(data_iter, net):
+    acc_sum, n = 0.0, 0
+    for _, (X, y) in enumerate(data_iter):
+        y = tf.cast(y,dtype=tf.int64)
+        acc_sum += np.sum(tf.cast(tf.argmax(net(X), axis=1), dtype=tf.int64) == y)
+        n += y.shape[0]
+    return acc_sum / n
