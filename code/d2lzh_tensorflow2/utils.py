05164fbb56a44822921c99233b2e52a1e85a1cc8
archersama <xiangyangli@pku.edu.cn>
2019-12-28 15:24:29 +0800

    upload 6.3 functions

--- code/d2lzh_tensorflow2/utils.py
+++ code/d2lzh_tensorflow2/utils.py
@@ -124,3 +124,59 @@ def evaluate_accuracy(data_iter, net):
         acc_sum += np.sum(tf.cast(tf.argmax(net(X), axis=1), dtype=tf.int64) == y)
         n += y.shape[0]
     return acc_sum / n
+  
+  
+  
+# ############################## 6.3 ##################################3
+def load_data_jay_lyrics():
+    """加载周杰伦歌词数据集"""
+    with zipfile.ZipFile('../../data/jaychou_lyrics.txt.zip') as zin:
+        with zin.open('jaychou_lyrics.txt') as f:
+            corpus_chars = f.read().decode('utf-8')
+    corpus_chars = corpus_chars.replace('\n', ' ').replace('\r', ' ')
+    corpus_chars = corpus_chars[0:10000]
+    idx_to_char = list(set(corpus_chars))
+    char_to_idx = dict([(char, i) for i, char in enumerate(idx_to_char)])
+    vocab_size = len(char_to_idx)
+    corpus_indices = [char_to_idx[char] for char in corpus_chars]
+    return corpus_indices, char_to_idx, idx_to_char, vocab_size
+  
+  
+def data_iter_random(corpus_indices, batch_size, num_steps, ctx=None):
+    # 减1是因为输出的索引是相应输入的索引加1
+    num_examples = (len(corpus_indices) - 1) // num_steps
+    epoch_size = num_examples // batch_size
+    example_indices = list(range(num_examples))
+    random.shuffle(example_indices)
+
+    # 返回从pos开始的长为num_steps的序列
+    def _data(pos):
+        return corpus_indices[pos: pos + num_steps]
+
+    for i in range(epoch_size):
+        # 每次读取batch_size个随机样本
+        i = i * batch_size
+        batch_indices = example_indices[i: i + batch_size]
+        X = [_data(j * num_steps) for j in batch_indices]
+        Y = [_data(j * num_steps + 1) for j in batch_indices]
+        yield np.array(X, ctx), np.array(Y, ctx)
+        
+        
+def data_iter_consecutive(corpus_indices, batch_size, num_steps, ctx=None):
+    corpus_indices = np.array(corpus_indices)
+    data_len = len(corpus_indices)
+    batch_len = data_len // batch_size
+    indices = corpus_indices[0: batch_size*batch_len].reshape((
+        batch_size, batch_len))
+    epoch_size = (batch_len - 1) // num_steps
+    for i in range(epoch_size):
+        i = i * num_steps
+        X = indices[:, i: i + num_steps]
+        Y = indices[:, i + 1: i + num_steps + 1]
+        yield X, Y
+
+  
+  
+  
+  
+  
